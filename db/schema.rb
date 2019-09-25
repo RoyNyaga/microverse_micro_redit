a8b3b79f31a0e13dbc939fbcf42de6b6823c2bcb
evaveskova <evaveskova@gmail.com>
2019-09-25 22:00:39 +0200

    generated post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_203834) do
+ActiveRecord::Schema.define(version: 2019_09_25_194332) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
