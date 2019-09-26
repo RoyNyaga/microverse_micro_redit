86a4f9bca03576b714e6eab0465096dc6c0d87b9
Roy Nyaga <nyagaandreroy@gmail.com>
2019-09-26 18:43:40 +0100

    adjusted erros detected by rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-	has_many :posts
-	has_many :comments
-	validates :name, presence: true, length: {minimum: 15}
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true, length: { minimum: 15 }
 end
